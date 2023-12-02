0ccd97a51f0c9174d4cbe64cd250b1012376c97c
BuildTools <unconfigured@null.spigotmc.org>
2023-12-02 12:06:24 +0900

    퀵정렬-과제

--- Sort/FileName.cpp
+++ Sort/FileName.cpp
@@ -8,138 +8,91 @@ int list[MAX_SIZE];
 int n;
 int move;
 int compare;
-int allmove;
-int allcompare;
+double allmove;
+double allcompare;
 
-//선택정렬(출력용)
-void selection_sort(int list[], int n)
+
+//퀵정렬(출력용)
+int partition(int list[], int left, int right)
 {
-	printf("Selection Sort\n");
-	move = 0;
-	compare = 0;
-	int i, j, least, temp;
-	for (i = 0; i < n - 1; i++)
-	{
-		least = i;
-		for (j = i + 1; j < n; j++) {		// 최솟값 탐색
-			if (list[j] < list[least]) least = j;
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = list[left];
+	do {
+		do {
+			low++;
 			compare++;
-		}
-		SWAP(list[i], list[least], temp);
+		} while (low <= right && list[low] < pivot);
+		do {
+			high--;
+			compare++;
+		} while (high >= left && list[high] > pivot);
+		if (low < high) SWAP(list[low], list[high], temp);
+		
 		move++;
-		for (int k = 0; k < n; k++)
-			printf("%d ", list[k]);
-		printf("\n");
-	}
-	printf("Move Count: %d\n", move);
-	printf("Compare Count: %d\n", compare);
+	} while (low < high);
+	SWAP(list[left], list[high], temp);
+	move++;
+	for (int o = 0; o < n; o++)
+		printf("%d ", list[o]);
+	printf("\n");
+	return high;
 }
 
 
-//선택정렬(평균비교용)
-void selection_sort1(int list[], int n)
+void quick_sort(int list[], int left, int right)
 {
-	move = 0;
+	if (left < right) {
+		int q = partition(list, left, right);
+		quick_sort(list, left, q - 1);
+		quick_sort(list, q + 1, right);
+	}
+}
+
+
+//퀵정렬(평균용)
+int partition1(int list[], int left, int right)
+{
 	compare = 0;
-	int i, j, least, temp;
-	for (i = 0; i < n - 1; i++) {
-		least = i;
-		for (j = i + 1; j < n; j++) {			// 최솟값 탐색
-			if (list[j] < list[least]) least = j;
+	move = 0;
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = list[left];
+	do {
+		do {
+			low++;
 			compare++;
-		}
-		SWAP(list[i], list[least], temp);
+		} while (low <= right && list[low] < pivot);
+		do {
+			high--;
+			compare++;
+		} while (high >= left && list[high] > pivot);
+		if (low < high) SWAP(list[low], list[high], temp);
+
 		move++;
-	}
+	} while (low < high);
+
+	SWAP(list[left], list[high], temp);
+	move++;
 	allmove = allmove + move;
 	allcompare = allcompare + compare;
-}
-
-//삽입정렬(출력용)
-void insertion_sort(int list[], int n)
-{
-	printf("Insert Sort\n");
-	move = 0;
-	compare = 0;
-	int i, j, key;
-	for (i = 1; i < n; i++) {
-		key = list[i];
-		for (j = i - 1; j >= 0 && list[j] > key; j--) {
-			list[j + 1] = list[j]; 		// 레코드의 오른쪽 이동
-			compare++;
-			move++;
-		}
-		list[j + 1] = key;
-		for (int k = 0; k < n; k++)
-			printf("%d ", list[k]);
-		printf("\n");
-	}
-	printf("Move Count: %d\n", move);
-	printf("Compare Count: %d\n", compare);
-}
-
-//삽입정렬(평균비교용)
-void insertion_sort1(int list[], int n)
-{
-	move = 0;
-	compare = 0;
-	int i, j, key;
-	for (i = 1; i < n; i++) {
-		key = list[i];
-		for (j = i - 1; j >= 0 && list[j] > key; j--) {
-			list[j + 1] = list[j]; 		// 레코드의 오른쪽 이동
-			compare++;
-			move++;
-		}
-		list[j + 1] = key;
-	}
-	allmove = allmove + move;
-	allcompare = allcompare + compare;
+	return high;
 }
 
 
-
-
-//버블정렬(출력용)
-void bubble_sort(int list[], int n)
+void quick_sort1(int list[], int left, int right)
 {
-	printf("Bubble Sort\n");
-	move = 0;
-	compare = 0;
-	int i, j, temp;
-	for (i = n - 1; i > 0; i--) {
-		for (j = 0; j < i; j++) { 	// 앞뒤의 레코드를 비교한 후 교체
-			compare++;
-			if (list[j] > list[j + 1]) {
-				SWAP(list[j], list[j + 1], temp);
-				move++;
-			}
-		}
-		for (int k = 0; k < n; k++)
-			printf("%d ", list[k]);
-		printf("\n");
+	if (left < right) {
+		int q = partition1(list, left, right);
+		quick_sort1(list, left, q - 1);
+		quick_sort1(list, q + 1, right);
 	}
-	printf("Move Count: %d\n", move);
-	printf("Compare Count: %d\n", compare);
-}
-
-//버블정렬(평균비교용)
-void bubble_sort1(int list[], int n)
-{
-	move = 0;
-	compare = 0;
-	int i, j, temp;
-	for (i = n - 1; i > 0; i--) {
-		for (j = 0; j < i; j++) {	// 앞뒤의 레코드를 비교한 후 교체
-			compare++;
-			if (list[j] > list[j + 1]) {
-				SWAP(list[j], list[j + 1], temp);
-				move++;
-			}
-		}
-	}
-	allmove = allmove + move;
-	allcompare = allcompare + compare;
 }
 
 
@@ -150,9 +103,8 @@ int main(void)
 	n = MAX_SIZE;
 	allmove = 0;
 	allcompare = 0;
-	
 
-	//선택정렬
+	//퀵정렬
 	srand(time(NULL));
 	for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
 		list[i] = rand() % 100; // 난수 발생 범위 0~99
@@ -161,60 +113,17 @@ int main(void)
 		printf("%d ", list[k]);
 	printf("\n\n");
 
+	quick_sort(list, 0, n - 1);
 
-	selection_sort(list, n); // 선택정렬 호출 
+	//퀵정렬의 20회 평균 구하기
 	for (int s = 0; s < 20; s++) {
 		for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
 			list[i] = rand() % 100; // 난수 발생 범위 0~99
-		selection_sort1(list, n);
+		quick_sort1(list, 0, n - 1);
 	}
-	printf("move평균: %d\n", allmove / 20);
-	printf("compare평균: %d\n", allcompare / 20);
-
-
-
-	printf("\n\n");
-
-	
-	//삽입정렬
-	allmove = 0;
-	allcompare = 0;
-	for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-		list[i] = rand() % 100; // 난수 발생 범위 0~99
-	printf("Original List\n");
-	for (int k = 0; k < n; k++)
-		printf("%d ", list[k]);
-	printf("\n\n");
-
-	insertion_sort(list, n);
-	for (int s = 0; s < 20; s++) {
-		for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-			list[i] = rand() % 100; // 난수 발생 범위 0~99
-		insertion_sort1(list, n);
-	}
-	printf("move평균: %d\n", allmove / 20);
-	printf("compare평균: %d\n", allcompare / 20);
-	printf("\n\n");
-
-
-	//버블정렬
-	allmove = 0;
-	allcompare = 0;
-	for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-		list[i] = rand() % 100; // 난수 발생 범위 0~99
-	printf("Original List\n");
-	for (int k = 0; k < n; k++)
-		printf("%d ", list[k]);
-	printf("\n\n");
-
-	bubble_sort(list, n);
-	for (int s = 0; s < 20; s++) {
-		for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-			list[i] = rand() % 100; // 난수 발생 범위 0~99
-		bubble_sort1(list, n);
-	}
-	printf("move평균: %d\n", allmove / 20);
-	printf("compare평균: %d\n", allcompare / 20);
+	//20회 평균 출력
+	printf("Average Move Count: %f\n", allmove / 20);
+	printf("Average Compare Count: %f\n", allcompare / 20);
 
 
 	return 0;
